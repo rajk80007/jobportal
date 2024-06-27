e54ca263c2a4733f80056a70b13acd0516600c7b
23shreysahni <23shreysahni@gmail.com>
2024-06-27 18:50:26 +0530

    login page added

--- resources/views/partials/Navbar.blade.php
+++ resources/views/partials/Navbar.blade.php
@@ -11,15 +11,13 @@
         <li><a href="/about" wire:navigate class="p-2 hover:font-bold hover:text-red-400 duration-300">About</a></li>
         <li><a href="#" wire:navigate class="p-2 hover:font-bold hover:text-red-400 duration-300">Services</a>
         </li>
-        <li><a href="#" wire:navigate class="p-2 hover:font-bold hover:text-red-400 duration-300">Contact</a></li>
+        <li><a href="/contact" wire:navigate class="p-2 hover:font-bold hover:text-red-400 duration-300">Contact</a></li>
     </ul>
     <div class="flex gap-2 px-5">
         <button
-            class="border border-white px-2 lg:px-3 py-1 rounded hover:bg-blue-600 bg-blue-500 hover:scale-105 hover:font-bold duration-400 text-sm lg:text-[0.9rem]">Log
-            In</button>
+            class="border border-white px-2 lg:px-3 py-1 rounded hover:bg-blue-600 bg-gradient-to-r from-black to-blue-500 hover:scale-105 hover:font-bold duration-400 text-sm lg:text-[0.9rem]"> <a href="/login/{{2}}" wire:navigate> Hire Now </a></button>
         <button
-            class="border border-white px-2  lg:px-3 py-1 rounded hover:bg-blue-600 bg-blue-500 hover:scale-105 hover:font-bold duration-400 text-sm lg:text-[0.9rem]">Sign
-            Up</button>
+            class="border border-white px-2  lg:px-3 py-1 rounded hover:bg-blue-600 bg-gradient-to-r from-blue-500 to-black hover:scale-105 hover:font-bold duration-400 text-sm lg:text-[0.9rem]"> <a href="/login/{{3}}" wire:navigate> Get a Job </a></button>
         <button class="hidden lg:block bg-white text-purple-900 font-bold hover:scale-105 px-3 py-1 rounded text-sm lg:text-[0.8] ">Post a Project</button>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use App\Livewire\About;
 use App\Livewire\HomePage;
+use App\Livewire\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class);
 Route::get('/about', About::class);
+
+Route::get('/login/{id}',[ 'uses'=>'AppController@getregistration','as' => 'login', 'uses' => Login::class]);
