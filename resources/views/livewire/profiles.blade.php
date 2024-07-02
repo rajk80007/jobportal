b08bf33aaf66173828c2f5e3afef13559605f738
23shreysahni <23shreysahni@gmail.com>
2024-07-02 18:48:59 +0530

    ddfs

--- resources/views/livewire/profiles.blade.php
+++ resources/views/livewire/profiles.blade.php
@@ -10,17 +10,16 @@
                 <div class="flex flex-col items-center justify-center w-[70%]">
                     <h4 class="text-3xl font-bold text-green-900">John Doe</h4>
                     <h6 class="text-xl text-gray-500 text-right w-full px-2">Web Developer</h6>
-                    <div class="flex justify-between">
-
-                        <h6 class="w-[100%] inline-block text-lg text-gray-500 text-left px-2">Skills :  </h6>
-                        <h6 class="inline-block text-lg text-gray-500 text-right w-[100%] px-2">Experience : <span>5 years</span> </h6>
-                    </div>
+                    <h6 class=" text-lg text-gray-500 text-right w-full px-2">Experience : <span>5 years</span> </h6>
+                    <h6 class="text-lg text-gray-500 text-left w-full  mt-[-20px]">Skills :  </h6>
+                   
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, ut? Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, soluta minima!</p>                    
                     
                 </div>
             </div>
         </div>
     </div>
+
     <div class="w-full text-black border-2 border-black shadow-xl fill-black shadow-white mx-auto my-2">
         <div class="text-black border-2 border-white shadow-xl fill-black p-2">
             <div class="flex justify-center items-start gap-5 bg-red-200">
@@ -32,10 +31,56 @@
                 <div class="flex flex-col items-center justify-center w-[70%]">
                     <h4 class="text-3xl font-bold text-green-900">John Doe</h4>
                     <h6 class="text-xl text-gray-500 text-right w-full px-2">Web Developer</h6>
-                    
+                    <h6 class=" text-lg text-gray-500 text-right w-full px-2">Experience : <span>5 years</span> </h6>
+                    <h6 class="text-lg text-gray-500 text-left w-full  mt-[-20px]">Skills :  </h6>
+                   
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, ut? Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, soluta minima!</p>                    
                     
                 </div>
             </div>
         </div>
     </div>
+
+    <div class="w-full text-black border-2 border-black shadow-xl fill-black shadow-white mx-auto my-2">
+        <div class="text-black border-2 border-white shadow-xl fill-black p-2">
+            <div class="flex justify-center items-start gap-5 bg-red-200">
+                <div class="flex flex-col items-center">
+
+                    <img src="images/profile.jpg" width="180" height="180" alt="user-img" class="p-4 rounded-md ">
+                    <button class="text-center w-full">hire me</button>
+                </div>
+                <div class="flex flex-col items-center justify-center w-[70%]">
+                    <h4 class="text-3xl font-bold text-green-900">John Doe</h4>
+                    <h6 class="text-xl text-gray-500 text-right w-full px-2">Web Developer</h6>
+                    <h6 class=" text-lg text-gray-500 text-right w-full px-2">Experience : <span>5 years</span> </h6>
+                    <h6 class="text-lg text-gray-500 text-left w-full  mt-[-20px]">Skills :  </h6>
+                   
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, ut? Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, soluta minima!</p>                    
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="w-full text-black border-2 border-black shadow-xl fill-black shadow-white mx-auto my-2">
+        <div class="text-black border-2 border-white shadow-xl fill-black p-2">
+            <div class="flex justify-center items-start gap-5 bg-red-200">
+                <div class="flex flex-col items-center">
+
+                    <img src="images/profile.jpg" width="180" height="180" alt="user-img" class="p-4 rounded-md ">
+                    <button class="text-center w-full">hire me</button>
+                </div>
+                <div class="flex flex-col items-center justify-center w-[70%]">
+                    <h4 class="text-3xl font-bold text-green-900">John Doe</h4>
+                    <h6 class="text-xl text-gray-500 text-right w-full px-2">Web Developer</h6>
+                    <h6 class=" text-lg text-gray-500 text-right w-full px-2">Experience : <span>5 years</span> </h6>
+                    <h6 class="text-lg text-gray-500 text-left w-full  mt-[-20px]">Skills :  </h6>
+                   
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam, ut? Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, soluta minima!</p>                    
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
